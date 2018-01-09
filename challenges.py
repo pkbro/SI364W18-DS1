193f1769127ea4ba97ddadd999ab028d0aa24b54
Phillip Brown <pkbro@umich.edu>
2018-01-09 16:27:45 -0500

    Change to course path

--- challenges.py
+++ challenges.py
@@ -14,7 +14,7 @@ def index():
 # Remember to get rid of the pass statement
 @app.route('/course/<course>')
 def course(course):
-   pass
+   return "Welcome to <h1>{}</h1>".format(course)
 
 # Task 3.1
 # Edit the HTML form such that form data is sent to localhost:5000/result using POST method
@@ -44,7 +44,7 @@ def displayData():
         pass
 
 ## Task 4
-## Note : Since this is a dyanmic URL, recipes function should recieve a paramter called `ingrdient` 
+## Note : Since this is a dyanmic URL, recipes function should recieve a paramter called `ingrdient`
 @app.route('/recipe/<ingredient>')
 def recipes():
     pass
